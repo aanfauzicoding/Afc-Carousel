866f251f057f317cfa7a05bff75cc4795e862e1b
Aan Fauzi <69013441+aanfauzicoding@users.noreply.github.com>
2022-04-11 01:15:10 +0700

    Add files via upload

---
+++ AFC-Carousel.js
@@ -0,0 +1,29 @@
+/*-----------------------------------------------
+
+Name    : AFC-Carousel Js
+Version : 1.0
+By      : Aan Fauzi
+https://github.com/aanfauzicoding/Afc-Carousel/blob/main/LICENSE
+
+----------------------------------------------- */
+
+eval(function (p, a, c, k, e, r) {
+    e = function (c) {
+        return (c < a ? '' : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))
+    };
+    if (!''.replace(/^/, String)) {
+        while (c--) r[e(c)] = k[c] || e(c);
+        k = [
+            function (e) {
+                return r[e]
+            }
+        ];
+        e = function () {
+            return '\\w+'
+        };
+        c = 1
+    };
+    while (c--)
+        if (k[c]) p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c]);
+    return p
+}('m b=["\\J\\K\\k\\x\\y\\g\\z\\n\\L\\f\\c\\e\\x\\M\\e\\q\\r\\c\\f","\\A\\c\\j\\N\\e\\c\\s\\c\\o\\j\\f\\O\\t\\y\\e\\g\\f\\f\\B\\g\\s\\c","\\r\\n\\j","\\e\\c\\o\\A\\j\\P","\\r\\q\\f\\C\\e\\g\\t","\\f\\j\\t\\e\\c","\\o\\n\\o\\c","\\k\\e\\g\\f\\f\\B\\g\\s\\c","\\Q\\g\\k\\j\\q\\R\\c","","\\z\\c\\C\\e\\g\\k\\c","\\S\\e\\n\\k\\T"];m l=u,d=0,D=!0;v w(){l-=E,1==D&&l<0&&F(),U(w,E)}v V(){l=u;m a=p[b[1]](b[0]),h=p[b[1]](b[2]);G(i=0;i<a[b[3]];i++){a[i][b[5]][b[4]]=b[6],h[i][b[7]]=h[i][b[7]][b[H]](b[8],b[9])};--d>a[b[3]]&&(d=1),0==d&&(d=3),a[d-1][b[5]][b[4]]=b[I],h[d-1][b[7]]+=b[8]}v F(){l=u;m a=p[b[1]](b[0]),h=p[b[1]](b[2]);G(i=0;i<a[b[3]];i++){a[i][b[5]][b[4]]=b[6],h[i][b[7]]=h[i][b[7]][b[H]](b[8],b[9])};++d>a[b[3]]&&(d=1),a[d-1][b[5]][b[4]]=b[I],h[d-1][b[7]]+=b[8]}w()', 58, 58, '|||||||||||_0x7df8|x65|slideIndex|x6C|x73|x61|_0x526cx7||x74|x63|timeOut|var|x6F|x6E|document|x69|x64|x6D|x79|2e3|function|autoSlides|x2D|x43|x72|x67|x4E|x70|autoOn|20|showSlides|for|10|11|x41|x66|x75|x53|x45|x42|x68|x20|x76|x62|x6B|setTimeout|prevSlide'.split('|'), 0, {}))
